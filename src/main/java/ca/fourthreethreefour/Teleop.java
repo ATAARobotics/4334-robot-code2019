7e0012121ffd3813cecc659ffa7445cfc670d1e7
Alexander Greco <33609792+Alexander4334@users.noreply.github.com>
2019-02-02 13:37:17 -0700

    Update and rename Teleop.java to src/main/java/ca/fourthreethreefour/Teleop.java

--- src/main/java/ca/fourthreethreefour/Teleop.java
+++ src/main/java/ca/fourthreethreefour/Teleop.java
@@ -11,7 +11,6 @@ public class Teleop {
     private Joystick gunnerStick = new Joystick(1);
     private Joystick driveStick = new Joystick(0);
 
-    private DoubleSolenoid hatchIntakeSolenoid = new DoubleSolenoid(0,1);
     private DoubleSolenoid gearShiftSolenoid = new DoubleSolenoid(2, 3);
     private VictorSPX rearLeftMotor = new VictorSPX(1);
     private TalonSRX frontLeftMotor = new TalonSRX(0);
@@ -19,11 +18,6 @@ public class Teleop {
     private TalonSRX frontRightMotor = new TalonSRX(2);
     private SWATDrive driveTrain = new SWATDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, gearShiftSolenoid);
     private boolean lowGear = true;
-    private Intake intake = new Intake(hatchIntakeSolenoid);
-
-    public void init() {
-        hatchIntakeSolenoid.set(DoubleSolenoid.Value.kOff);
-    }
 
 
     public void TeleopPeriodic() {
@@ -32,7 +26,6 @@ public class Teleop {
             lowGear = !lowGear;
             driveTrain.gearShift(lowGear);
           }
-        intake.IntakePeriodic(gunnerStick.getRawButton(5), gunnerStick.getRawButton(6));
           if (driveStick.getRawButton(1)) {
             driveTrain.slow = !driveTrain.slow;
           } 
