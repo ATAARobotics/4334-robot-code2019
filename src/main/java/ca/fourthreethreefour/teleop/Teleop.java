704c1326c91f3bc163ca5ad4837c28eba38a5cea
Kulkinz <cool@coolspages.com>
2019-02-07 19:47:46 -0700

    Added the classes needed for Cargo

---
+++ src/main/java/ca/fourthreethreefour/teleop/Teleop.java
@@ -0,0 +1,15 @@
+package ca.fourthreethreefour.teleop;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Teleop {
+
+
+  //Creates and initializes joystick objects and assigns them
+  //to their respective ports on the Driver Station
+  private Joystick gunnerStick = new Joystick(1);
+  private Joystick driveStick = new Joystick(0);
+
+  public void TeleopPeriodic() {
+  }
+}
