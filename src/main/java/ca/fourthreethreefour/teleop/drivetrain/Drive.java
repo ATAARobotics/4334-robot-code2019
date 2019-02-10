ff9de3ba9fb135b71a873ef7ed1c4042b8ebff01
unknown <acjparadis@outlook.com>
2019-02-09 19:53:03 -0500

    minor update

--- src/main/java/ca/fourthreethreefour/teleop/drivetrain/Drive.java
+++ src/main/java/ca/fourthreethreefour/teleop/drivetrain/Drive.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package ca.fourthreethreefour.teleop.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
@@ -15,10 +8,6 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
-/**
- * Add your docs here.
- */
 
 public class Drive extends Subsystem {
 
@@ -34,7 +23,6 @@ public class Drive extends Subsystem {
     private DifferentialDrive driveTrain = new DifferentialDrive(leftSpeedControllerGroup, rightSpeedControllerGroup);
     //Sets the boolean lowGear equal to true
     private boolean lowGear = true;
-    private boolean slow = false;
 
   @Override
   public void initDefaultCommand() {
