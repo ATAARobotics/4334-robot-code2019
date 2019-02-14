c2b2e1a08b3600496aae93e146ffcab1bc3a06ca
brianvo <brianvo125@gmail.com>
2019-02-13 18:46:20 -0700

    Connecting to shuffleboard and added accessor functions

--- src/main/java/ca/fourthreethreefour/teleop/systems/Ultrasonics.java
+++ src/main/java/ca/fourthreethreefour/teleop/systems/Ultrasonics.java
@@ -1,8 +1,10 @@
 package ca.fourthreethreefour.teleop.systems;
 
+import ca.fourthreethreefour.commands.debug.Logging;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Ultrasonics extends Subsystem {
 
@@ -12,14 +14,23 @@ public class Ultrasonics extends Subsystem {
     Ultrasonic ultrasonicSensorBack = new Ultrasonic(0, 1);
     Ultrasonic ultrasonicSensorRight = new Ultrasonic(6, 7);
 
+  /** Constructor for initalizing
+   * @return void
+   */
+    public Ultrasonics () {
+        setAutomaticMode(true);
+        ultrasonicPollingThread();
+    }
+
     // Function to start a new thread to poll the ultrasonic sensors
-    public void ultrasonicPollingThread()
+    private void ultrasonicPollingThread()
     {
         // Sets up a new thread that polls at a set interval
         Thread thread = new Thread(() -> {
             while (!Thread.interrupted())
             {
                 // Pings the ultrasonic sensors
+  
                 ultrasonicSensorFront.ping();
                 ultrasonicSensorLeft.ping();
                 ultrasonicSensorBack.ping();
@@ -39,8 +50,30 @@ public class Ultrasonics extends Subsystem {
         ultrasonicSensorRight.setEnabled(true);
     }
 
+ /** Enables automatic mode on sensors
+   *  @param enabling mode to set
+   * @return void
+   */
+    private void setAutomaticMode(boolean enabling){
+        ultrasonicSensorFront.setAutomaticMode(true);
+        ultrasonicSensorLeft.setAutomaticMode(true);
+        ultrasonicSensorBack.setAutomaticMode(true);
+        ultrasonicSensorRight.setAutomaticMode(true);
+    }
+
+  /**
+   *  Logging to shuffleboard
+   * @return void
+   */
+    public void updateShuffleboard()
+    {
+        Logging.log("Ultrasonic Front" + String.valueOf((ultrasonicSensorFront.isRangeValid() == false) ? SmartDashboard.getNumber("Ultrasonic Front", 999.0) : ultrasonicSensorFront.getRangeInches()));
+        Logging.log("Ultrasonic Left" + String.valueOf((ultrasonicSensorLeft.isRangeValid() == false) ? SmartDashboard.getNumber("Ultrasonic Left", 999.0) : ultrasonicSensorLeft.getRangeInches()));
+        Logging.log("Ultrasonic Back" + String.valueOf((ultrasonicSensorBack.isRangeValid() == false) ? SmartDashboard.getNumber("Ultrasonic Back", 999.0) : ultrasonicSensorBack.getRangeInches()));
+        Logging.log("Ultrasonic Right"+ String.valueOf( (ultrasonicSensorRight.isRangeValid() == false) ? SmartDashboard.getNumber("Ultrasonic Right", 999.0) : ultrasonicSensorRight.getRangeInches()));
+    }
     @Override
     protected void initDefaultCommand() {
-
+  
     }
 }
