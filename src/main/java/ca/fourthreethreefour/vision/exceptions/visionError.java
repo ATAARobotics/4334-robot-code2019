fb633c81097c9592b02d0e1987971d8d4afe5890
Matthew naruzny <matthew_naruzny@shaw.ca>
2019-02-21 16:09:21 -0700

    Created Exceptions

---
+++ src/main/java/ca/fourthreethreefour/vision/exceptions/visionError.java
@@ -0,0 +1,8 @@
+package ca.fourthreethreefour.vision.exceptions;
+public class visionError extends Exception{
+    private static final long serialVersionUID = 5263405110678933661L;
+
+    public visionError(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ca/fourthreethreefour/vision/exceptions/visionTargetException.java
@@ -0,0 +1,8 @@
+package ca.fourthreethreefour.vision.exceptions;
+public class visionTargetException extends Exception{
+    private static final long serialVersionUID = 2398276135238940559L;
+
+    public visionTargetException(String message) {
+        super(message);
+    }
+}
