6ea6e8025ec5d52dfc100b3088a5c04730598082
Matthew Naruzny <matthew_naruzny@shaw.ca>
2019-02-26 21:17:32 -0700

    Renamed Exception

--- src/main/java/ca/fourthreethreefour/vision/exceptions/visionTargetDetectionException.java
+++ src/main/java/ca/fourthreethreefour/vision/exceptions/visionTargetDetectionException.java
@@ -1,8 +1,8 @@
 package ca.fourthreethreefour.vision.exceptions;
-public class visionTargetException extends Exception{
+public class visionTargetDetectionException extends Exception{
     private static final long serialVersionUID = 2398276135238940559L;
 
-    public visionTargetException(String message) {
+    public visionTargetDetectionException(String message) {
         super(message);
     }
 }
