2c6f669ad2e9679cf281ed4a266d9674ea4afa07
Kulkinz <cool@coolspages.com>
2019-02-28 20:50:31 -0700

    Updated offset and added printing

--- src/main/java/ca/fourthreethreefour/teleop/systems/Encoders.java
+++ src/main/java/ca/fourthreethreefour/teleop/systems/Encoders.java
@@ -6,9 +6,19 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 public class Encoders {
+
+    double potentiometerOffset;
+
+    public Encoders(double offSet) {
+        potentiometerOffset = offSet;
+    }
     
     public DigitalInput cargoButton = new DigitalInput(Settings.CARGO_BUTTON_PORT);
     public DigitalInput armInnerLimitSwitch = new DigitalInput(2);
 
-    public Potentiometer armPotentiometer = new AnalogPotentiometer(0, 1800, -1520);
+    public Potentiometer armPotentiometer = new AnalogPotentiometer(0, 1800, -1506);
+
+    public void printPotentiometer() {
+        System.out.println(armPotentiometer.get());
+    }
 }
